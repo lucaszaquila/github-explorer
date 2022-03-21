72e6ae2b14af1c94cdf06e2e33bb8862cfe0d1d2
Lucas Áquila <lucaszaquila@gmail.com>
2022-03-21 14:49:47 -0300

    Consumindo API do Github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,21 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-//https://api.github.com/users/lucaszaquila/repos
-
-const repository = {
-    name: 'unform',
-    description: 'Forms in react',
-    link: 'https://github.com/'
-}
-
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/lucaszaquila/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
     return (
         <section className="repository-list">
             <h1>Lista de repósitorios</h1>
             <ul>
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
+                { repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />) }
             </ul>
         </section>
     )
